47cadc7ce8fdafbd5699870a4f1224d8796614ea
PSH <packtty18@naver.com>
2025-12-08 18:26:24 +0900

    Feat : 카메라와 플레이어의 이동 및 회전
    PlayerController 사용하기
    카메라 회전
    플레이어 이동 및 회전

---
+++ Assets/02.Scripts/02.Player/PlayerMove.cs
@@ -0,0 +1,52 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private const float GRAVITY = -9.81f;
+
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _DashSpeed = 10;
+    [SerializeField] private float _jumpPower = 5f;
+
+    [SerializeField] private float _stamina = 100;
+    [SerializeField] private float _staminaRecoveryRatePerSecond = 1;
+    [SerializeField] private float _staminaDelayTime = 3;
+
+    
+    private float _yVelocity;
+    private bool _isDash => Input.GetKey(KeyCode.LeftShift);
+    private void Awake()
+    {
+        if(TryGetComponent(out CharacterController characterController))
+        {
+            _characterController = characterController;
+        }
+        else
+        {
+            transform.AddComponent<CharacterController>();
+            _characterController = transform.GetComponent<CharacterController>();
+        }
+    }
+    private void Update()
+    {
+        _yVelocity += GRAVITY * Time.deltaTime;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v).normalized;
+        direction = Camera.main.transform.TransformDirection(direction);
+
+        if(Input.GetButtonDown("Jump") && _characterController.isGrounded)
+        {
+            _yVelocity = _jumpPower;
+        }
+        direction.y = _yVelocity;
+
+
+        float MoveSpeed = _isDash ? _DashSpeed : _moveSpeed;
+        _characterController.Move(direction * _moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/02.Scripts/02.Player/PlayerRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    [SerializeField] private float _sensitivity = 200f;
+
+    //변화를 -360~360으로 누적할 변수
+    private float _accumulateX = 0;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+
+        float mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        _accumulateX += mouseX * _sensitivity * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(0, _accumulateX, 0);
+
+    }
+}
