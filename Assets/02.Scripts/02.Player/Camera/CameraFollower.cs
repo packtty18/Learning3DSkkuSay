fbddc4c04406048e10ed142ef89efbbd46fd695c
PSH <packtty18@naver.com>
2025-12-08 18:26:24 +0900

    Feat : 카메라와 플레이어의 이동 및 회전
    PlayerController 사용하기
    카메라 회전
    플레이어 이동 및 회전

---
+++ Assets/02.Scripts/02.Player/Camera/CameraFollower.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private void LateUpdate()
+    {
+        transform.position = _target.transform.position;
+    }
+}
