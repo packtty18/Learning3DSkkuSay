d78742c7d4a56014efb9d9dbeec3461fa971e5e5
PSH <packtty18@naver.com>
2025-12-08 14:31:46 +0900

    Chore : 에러 학습 추가

---
+++ Assets/02.Scripts/Learning/ErrorLearning/ErrorTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class ErrorTest1 : MonoBehaviour
+{
+    public GameObject target;
+
+
+    private void Start()
+    {
+        //NullReferenceException : 참조하려는 객체가 Null
+        Debug.Log(target.name);
+
+
+        //MissingComponentException : 참조하려는 컴포넌트가 적용되어 있지 않음
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        if(rigidbody2D == null)
+        {
+            return;
+        }
+
+        //or
+
+        if (TryGetComponent(out Rigidbody2D rigidbody))
+        {
+            return;
+        }
+
+    }
+}
---
+++ Assets/02.Scripts/Learning/ErrorLearning/ErrorTest2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ErrorTest2 : MonoBehaviour
+{
+    private void Start()
+    {
+        //missingReferenceException : 삭제된 게임 오브젝트에 접근할경우
+        Destroy(gameObject);
+        Debug.Log(gameObject.name);
+
+        //IndexOutOfRangeException : 배열이나 리스트에서 유효하지 않은 인덱스 접근시
+        int[] nums = new int[10];
+        Debug.Log(nums[11]);
+
+        //DevideByZeroException : 0으로 나눠질경우 에러
+        int num = 10;
+        int zero = 0;
+        Debug.Log(num/zero);
+    }
+}
