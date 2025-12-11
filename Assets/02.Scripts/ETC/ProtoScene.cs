1d62ff286cea5147cbde6a8307ba76a7259a7e2e
PSH <packtty18@naver.com>
2025-12-11 17:33:41 +0900

    Feat : 마우스 고정

---
+++ Assets/02.Scripts/ETC/ProtoScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ProtoScene : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
