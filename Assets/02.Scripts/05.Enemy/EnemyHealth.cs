5afa8b0efae8058a43266bcba2e8e0a0ed6237fb
PSH <packtty18@naver.com>
2025-12-12 10:21:45 +0900

    Feat : 적의 State, 이동 과 공격, 피격 생성

---
+++ Assets/02.Scripts/05.Enemy/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float current;
+
+    private void Awake()
+    {
+        current = maxHealth;
+    }
+
+
+    public bool ApplyDamage(float damage)
+    {
+        current -= damage;
+        DebugManager.Instance.Log($"피격 발생 : {gameObject.name}에게 {damage} 데미지. 남은체력 : {current} / {maxHealth}");
+        return current > 0;
+    }
+}
