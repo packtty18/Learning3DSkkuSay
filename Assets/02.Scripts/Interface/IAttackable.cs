355c0711b3f66488bb435f48a13fad6335f9b792
PSH <packtty18@naver.com>
2025-12-12 10:20:58 +0900

    Feat : 공격과 피격의 인터페이스 생성

---
+++ Assets/02.Scripts/Interface/IAttackable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public interface IAttackable
+{
+    void Attack(AttackData data);
+}
+
+public struct AttackData
+{
+    public float Damage; //데미지
+    public Vector3 HitDirection; //공격 방향 
+    public GameObject Attacker;  //공격의 주체
+
+    public AttackData(float damage, Vector3 hitDirection, GameObject attacker)
+    {
+        Damage = damage;
+        HitDirection = hitDirection;
+        Attacker = attacker;
+    }
+}
