7a869c11fad3b4d438527b7293c3d12e41f14cd3
PSH <packtty18@naver.com>
2025-12-15 10:32:05 +0900

    Feat : 적 체력 UI 제작

---
+++ Assets/02.Scripts/ETC/BillboardForUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+//빌보드 기능
+public class BillboardForUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _camera;
+
+    [Header("Options")]
+    [SerializeField] private bool _lockX = false;
+    [SerializeField] private bool _lockY = false;
+    [SerializeField] private bool _lockZ = false;
+
+    private void Awake()
+    {
+        if (_camera != null)
+        {
+            return;
+        }
+
+        Camera mainCam = Camera.main;
+        if (mainCam != null)
+        {
+            _camera = mainCam.transform;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_camera == null)
+        {
+            return;
+        }
+
+        Vector3 direction = transform.position - _camera.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+        Vector3 euler = lookRotation.eulerAngles;
+
+        if (_lockX)
+        {
+            euler.x = transform.eulerAngles.x;
+        }
+        if (_lockY)
+        {
+            euler.y = transform.eulerAngles.y;
+        }
+        if (_lockZ)
+        {
+            euler.z = transform.eulerAngles.z;
+        }
+
+        transform.rotation = Quaternion.Euler(euler);
+    }
+}
