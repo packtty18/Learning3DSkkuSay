5015f7dae9ff29f00daa95abaf8a1ca56dbdd3ca
PSH <packtty18@naver.com>
2025-12-15 15:24:49 +0900

    Feat : 미니맵 + 피격 오버레이

---
+++ Assets/02.Scripts/01.Managers/CursurManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorManager : Singleton<CursorManager>
+{
+    private bool _isUIActive;
+
+    public override void Init()
+    {
+        _isUIActive = false; 
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetCursurMode();
+        }
+    }
+
+    public void SetCursurMode()
+    {
+        _isUIActive = !_isUIActive;
+
+        if (_isUIActive)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public bool IsPointerOverUI()
+    {
+        return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+    }
+
+    
+}
+
