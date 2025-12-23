776f9bf44ef24dc7444d8dc87c9fd4405142bd20
PSH <packtty18@naver.com>
2025-12-23 17:00:11 +0900

    Feat : 로딩씬추가

---
+++ Assets/02.Scripts/06.Scene/LoadingScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] private Slider _progressBar;
+    [SerializeField] private TextMeshProUGUI _progressText;
+
+    private void Start()
+    {
+        StartCoroutine(Loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        AsyncOperation ao =  SceneManager.LoadSceneAsync(2);
+
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            _progressBar.value = ao.progress;
+            _progressText.text = $"{ao.progress * 100}%";
+
+            if(ao.progress >= 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
